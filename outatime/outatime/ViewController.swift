e75ddb6e9f0450b8abe1734e57633425e1c6aef2
Pravin Patel <patelpra337@gmail.com>
2019-12-11 20:06:48 -0600

    Create project, & storyboard layout

---
+++ outatime/outatime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  outatime
+//
+//  Created by patelpra on 12/11/19.
+//  Copyright © 2019 Crus Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
