949496793ea3487c797b3aecca4e46030df8ea17
Pravin Patel <patelpra337@gmail.com>
2019-12-12 20:33:39 -0600

    Add IBOutlets and dateFormatter property to TimeCircuitsViewController

--- outatime/outatime/View Controllers/TimeCircuitsViewController.swift
+++ outatime/outatime/View Controllers/TimeCircuitsViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class TimeCircuitsViewController: UIViewController {
-
+    
+    @IBOutlet weak var destinationTimeLabel: UILabel!
+    @IBOutlet weak var presentTimeLabel: UILabel!
+    @IBOutlet weak var lastTimeDepartedLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    
+    var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss:SS"
+        dateFormatter.timeZone = TimeZone.init(secondsFromGMT: 0)
+        return dateFormatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
