d39e7d44ed3948a4d007286f66e3217735310d77
Pravin Patel <patelpra337@gmail.com>
2019-12-12 21:39:55 -0600

    Add IBAction and update TimeCircuitsViewController

--- outatime/outatime/View Controllers/TimeCircuitsViewController.swift
+++ outatime/outatime/View Controllers/TimeCircuitsViewController.swift
@@ -22,21 +22,69 @@ class TimeCircuitsViewController: UIViewController {
         return dateFormatter
     }()
     
+    var currentSpeed = 0
+    
+    private var timer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "ModalDestinationDatePickerSegue" {
+            if let datePickerVC = segue.destination as? DatePickerViewController {
+                datePickerVC.delegate = self
+            }
+        }
     }
-    */
+    
+    @IBAction func travelBackButtonTapped(_ sender: Any) {
+        if destinationTimeLabel.text != presentTimeLabel.text {
+            startTimer()
+        } else {
+            let alert = UIAlertController(title: "Time Travel Failed", message: "You are already in \(destinationTimeLabel.text!)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: updateSpeed(time:))
+    }
+    
+    private func resetTimer() {
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    private func updateSpeed(time: Timer) {
+        if currentSpeed >= 88 {
+            resetTimer()
+            lastTimeDepartedLabel.text = presentTimeLabel.text
+            presentTimeLabel.text = destinationTimeLabel.text
+            currentSpeed = 0
+            speedLabel.text = "\(currentSpeed) MPH"
+            showAlert()
+        } else {
+            currentSpeed += 1
+            speedLabel.text = "\(currentSpeed) MPH"
+        }
+    }
+    
+    private func showAlert() {
+        let alert = UIAlertController(title: "Time Travel Successful", message: "You are now in \(presentTimeLabel.text!)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
 
+}
+
+extension TimeCircuitsViewController: DatePickerDelegate {
+    func destinationDateWasChosen(date: Date) {
+        destinationTimeLabel.text = dateFormatter.string(from: date)
+    }
 }
